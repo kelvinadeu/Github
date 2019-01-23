8b61f4831c5bc8636f63f9862a55b7a78b2833bd
kelvinadeu <kelvinadeu25@gmail.com>
2019-01-23 11:20:24 +0300

    add to page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
--- src/app/git-profile/git-profile.component.ts
+++ src/app/git-profile/git-profile.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import {GhProfileService} from '../gh-profile.service';
+import {HttpClient} from '@angular/common/http';
+import {Profile} from '../profile';
+import {Repo} from '../repo';
 
 @Component({
   selector: 'app-git-profile',
   templateUrl: './git-profile.component.html',
+
   styleUrls: ['./git-profile.component.css']
 })
 export class GitProfileComponent implements OnInit {
+  profile:Profile;
+  repo:Repo;
+  reposArray:any[];
+  public username:string;
 
   constructor() { }
 
+  searchProfil(){
+    this.gitProfile.profileRequest();
+    this.profile = this.gitProfile.profile;
+    this.gitProfile.repoRequest();
+    this.reposArray=this.gitProfile.reposArray;
+  }
+
   ngOnInit() {
+    this.gitProfile.profileRequest();
+    this.profile=this.gitProfile.profileRequest;
+    this.gitProfile.repoRequest();
+    this.reposArray=this.gitProfile.reposArray;
   }
 
 }
