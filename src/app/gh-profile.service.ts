66f6fd4b7f3e8fba8739558300a4371c613cb853
kelvinadeu <kelvinadeu25@gmail.com>
2019-01-23 13:33:01 +0300

    add to page

--- src/app/gh-profile.service.ts
+++ src/app/gh-profile.service.ts
@@ -1,51 +1,58 @@
 import { Injectable } from '@angular/core';
-import {Profile} from './profile';
-import {Repo} from './repo';
-import {environment} from '../environments/environment';
-import {HttpClient} from '@angular/common/http';
-import {Http, Headers} from '@angular/http';
+import { Profile } from './profile';
+import { Repo } from './repo';
+import { environment } from '../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Http, Headers } from '@angular/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GhProfileService {
 
-  private username:string;
-  profile:Profile;
-  repos:Repo;
-  reposArray:any;
+  private username: string;
+  profile: Profile;
+  repos: Repo;
+  reposArray: any;
   BaseUrl = environent.BaseUrl;
-  FinalUrl =environment.FinalUrl;
+  FinalUrl = environment.FinalUrl;
 
 
   constructor() {
-    interface ApiResponse{
-      name:string;
-      login:string;
-      location:string;
-      bio:string;
-      email:string;
-      followers:number;
-      following:number;
-      public_repos:number;
-      html_url:string;
+    interface ApiResponse {
+      name: string;
+      login: string;
+      location: string;
+      bio: string;
+      email: string;
+      followers: number;
+      following: number;
+      public_repos: number;
+      html_url: string;
     }
-    let promise = new Promise((resolve,reject))=>{
-      this.http.get(this.BaseUrl+this.username+this.FinalUrl).toPromise().then(response=>{
+    let promise = new Promise((resolve, reject))=> {
+      this.http.get(this.BaseUrl + this.username + this.FinalUrl).toPromise().then(response => {
         this.profile.name = response.json().name;
-        this.profile.login =response.json().login;
-        this.profile.location =response.json().location;
-        this.profile.avatar_url =response.json().avatar_url;
-        this.profile.bio =response.json().bio;
-        this.profile.blog =response.json().blog;
-        this.profile.email =response.json().email;
-        this.profile.followers =response.json().followers;
-        this.profile.following =response.json().following;
-        this.profile.public_repos =response.json().public_repos;
-        this.profile.html_url =response.json().html_url;
+        this.profile.login = response.json().login;
+        this.profile.location = response.json().location;
+        this.profile.avatar_url = response.json().avatar_url;
+        this.profile.bio = response.json().bio;
+        this.profile.blog = response.json().blog;
+        this.profile.email = response.json().email;
+        this.profile.followers = response.json().followers;
+        this.profile.following = response.json().following;
+        this.profile.public_repos = response.json().public_repos;
+        this.profile.html_url = response.json().html_url;
         resolve()
-        console
+        console.log("Working");
+        console.log(this.profile);
+      },
+      erro=>{
+        console.log("Error occured")
+        reject(error);
       })
+      })
+      return promise;
     }
   }
 }
