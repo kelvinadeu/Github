aa353dac20d7aaabcece760b3a4364a06703f52d
kelvinadeu <kelvinadeu25@gmail.com>
2019-01-23 12:21:51 +0300

    add to page

---
+++ src/app/bolden.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import {Profile} from './profile';
+import {Repo} from './repo';
+
+@Pipe({
+  name: 'bolden'
+})
+export class BoldenPipe implements PipeTransform {
+
+  transform(value:string): any {
+    return value.toUpperCase();
+  }
+
+}
