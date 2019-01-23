fd610c2857834a520df342ee73383bb45ab5d7d8
kelvinadeu <kelvinadeu25@gmail.com>
2019-01-23 11:27:10 +0300

    add to page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GitProfileComponent } from './git-profile/git-profile.component';
+import { IntroComponent } from './intro/intro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GitProfileComponent
+    GitProfileComponent,
+    IntroComponent
   ],
   imports: [
     BrowserModule,
