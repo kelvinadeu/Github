9ad066a304e7a857ac600936aea2610ad5a33cc7
kelvinadeu <kelvinadeu25@gmail.com>
2019-01-23 14:58:27 +0300

    add to page

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  BaseUrl:"https//api.github.com/users/",
+  FinalUrl:"?client_secret=2e5e14352a2394ac3049e179518b438a9a94968b"
 };
