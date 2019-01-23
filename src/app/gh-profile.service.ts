84e03436da6dad762ec168cd20a8c77afe5d4dd3
kelvinadeu <kelvinadeu25@gmail.com>
2019-01-23 13:49:00 +0300

    add to page

--- src/app/gh-profile.service.ts
+++ src/app/gh-profile.service.ts
@@ -54,5 +54,25 @@ export class GhProfileService {
       })
       return promise;
     }
+
+    repoRequest(){
+      this.reposArray = [];
+      interface ApiResponse{
+        name:string;
+        html_url:string;
+        description:string;
+      }
+      let promise = new Promise(resolve,reject)=>{
+        this.http.get(this.BaseUrl + this.username +"/repos" + this.FinalUrl).toPromise().then(response =>{
+          for (let repo of response.json()){
+            this.repos.name = repo.name;
+            this.repos.html_url = repo.html_url;
+            this.repos.description = repo.description;
+            this.reposArray.push(this.repos);
+            this.repos = new Repo("","","");
+          }
+        })
+      }
+    }
   }
 }
