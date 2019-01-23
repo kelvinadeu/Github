8a28512cdf0f3d2981d3414101eb6d3a368c43eb
kelvinadeu <kelvinadeu25@gmail.com>
2019-01-23 12:00:25 +0300

    add to page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {Environment} from '../environments/environment';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
---
+++ src/app/routing/routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {routerModule,Routes} from '@angular/router';
+
+import {GitProfileComponent} from '../git-profile/git-profile.component';
+import {IntroComponent} from '../intro/intro.component';
+import {PageNotFoundomponent} from '../page-not-found/page-not-found.component';
+
+const routes:Routes = [
+  {path."git-profile",component:GitProfileComponent},
+  {path:"intro",component:IntroComponent},
+  {path:"",redirect:"/intro",pathMatch:"full"},
+  {path:"**",Component:PageNotFoundomponent}
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    routerModule.forRoot(routes)
+  ],
+  exports:[RouterModule],
+  declarations:[]
+})
+export class RoutingModule { }
