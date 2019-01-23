8b2b8932af490d3f6b1b131c2bf21b70c255399b
kelvinadeu <kelvinadeu25@gmail.com>
2019-01-23 12:57:42 +0300

    add to page

--- src/app/gh-profile.service.ts
+++ src/app/gh-profile.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
-
+import {Profile} from './profile';
+import {Repo} from './repo';
+import {environment} from '../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {Http, Headers} from '@angular/http';
 @Injectable({
   providedIn: 'root'
 })
